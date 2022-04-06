167a556be7b68451267b13331e1d7b8cd24fd2f4
Алия Шамирова <Shamirowaaliam@gmail.com>
2022-04-06 17:14:06 +0300

    Cell свойства

---
+++ GameSaper.Domain/Cell.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameSaper.Domain
+{
+    public class Cell //Ячейки
+    {
+        public bool IsBomb { get; set; }
+        public bool WithFlag { get; set; }
+        public int VerticalPosition { get; set; }
+        public int HorizontalPosition { get; set; }
+    }
+}
