26f8694abdeb14952b905012f8a443c4a8545dba
Алия Шамирова <Shamirowaaliam@gmail.com>
2022-08-17 17:14:05 +0300

    Переделка отображение на Blazor Server

--- GameSaper.Web3/Models/FieldSize.cs
+++ GameSaper.Web3/Models/FieldSize.cs
@@ -4,12 +4,13 @@ namespace GameSaper.Web3.Models
 {
     public class FieldSize
     {
-        [Required] //Значение не может быть нулевым
-        [Range(5, 15, ErrorMessage = "Минимальное значение 5. Максимальное значение 15!")]
+        //Выполнение валидации введённых данных, а именно значение не может быть нулевым
+        [Required]
+        [Range(5, 15, ErrorMessage = "Минимальное значение 5. Максимальное значение 15.")]
         public int Height { get; set; }
 
         [Required]
-        [Range(5, 15, ErrorMessage = "Минимальное значение 5. Максимальное значение 15!")]
+        [Range(5, 15, ErrorMessage = "Минимальное значение 5. Максимальное значение 15.")]
         public int Width { get; set; }
     }
 }
