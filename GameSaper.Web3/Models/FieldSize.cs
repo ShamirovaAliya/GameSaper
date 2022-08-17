e3000d3072697548279441b7b61373dc702390f1
Алия Шамирова <Shamirowaaliam@gmail.com>
2022-08-17 13:58:56 +0300

    Переделка отображений на Blazor Server

---
+++ GameSaper.Web3/Models/FieldSize.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GameSaper.Web3.Models
+{
+    public class FieldSize
+    {
+        [Required] //Значение не может быть нулевым
+        [Range(5, 15, ErrorMessage = "Минимальное значение 5. Максимальное значение 15!")]
+        public int Height { get; set; }
+
+        [Required]
+        [Range(5, 15, ErrorMessage = "Минимальное значение 5. Максимальное значение 15!")]
+        public int Width { get; set; }
+    }
+}
