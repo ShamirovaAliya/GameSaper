afb3badbc99be8cdb20c6ff8db1c262ddf76423d
Алия Шамирова <Shamirowaaliam@gmail.com>
2022-09-14 18:33:48 +0300

    Revert "Deploying to shamirova/win-indication-fix from @ ShamirovaAliya/GameSaper@82604566cf85213505772fbe37d2bd4840b1ee8a 🚀"
    This reverts commit 79979eca7801c71c72b4416fad3b62a20f3d2c72.

---
+++ GameSaper.Domain/Row.cs
@@ -0,0 +1,22 @@
+﻿namespace GameSaper.Domain
+{
+    public class Row //Ряд
+    {
+        //Метод, для определения ячеек
+        public Row(int cellNumber, int row)
+        {
+            Cells = new Cell[cellNumber];
+            for (int i = 0; i < Cells.Length; i++)
+            {
+                Cells[i] = new Cell()
+                {
+                    Id = Guid.NewGuid().ToString(), //Уникальный идентификатор, чтобы можно было обозначить каждую ячейку
+                    Column = i,
+                    Row = row,
+                };
+            }
+        }
+
+        public Cell[] Cells { get; }
+    }
+}
