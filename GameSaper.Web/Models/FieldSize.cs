2625c4c3ca238252bb78fe41536d7d99581d2449
Алия Шамирова <Shamirowaaliam@gmail.com>
2022-09-14 13:45:06 +0300

    Переделка на WebAssembly

---
+++ GameSaper.Web/Models/FieldSize.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GameSaper.Web.Models
+{
+    public class FieldSize
+    {
+        //Выполнение валидации введённых данных, а именно значение не может быть нулевым
+        [Required]
+        [Range(5, 15, ErrorMessage = "Минимальное значение 5. Максимальное значение 15.")]
+        public int Height { get; set; }
+
+        [Required]
+        [Range(5, 15, ErrorMessage = "Минимальное значение 5. Максимальное значение 15.")]
+        public int Width { get; set; }
+
+        [Required]
+        public int NumberBombs { get; set; }
+    }
+}
